37eede1f616efa6b8b7a3bcbf33f08f7e8da4267
João Matheus <joaomatheusantunes@gmail.com>
2022-05-10 12:48:44 -0300

    firts

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const formResponse = document.querySelector(".form-response");
 const boxSelect = document.querySelector(".input-box-select");
 const options = document.querySelector(".options");
 const content = document.querySelector(".content");
+const error = document.querySelector('.error');
 
 inputResponse1.addEventListener('change', ()=>{
     const value = inputResponse1.value;
@@ -15,13 +16,26 @@ inputResponse1.addEventListener('change', ()=>{
         responseCorrect1.classList.add('response-correct-on-1');
         responseCorrect1.innerHTML = value;
 
+    }else{
+        error.classList.remove('error');
+        error.classList
+        .add('error-on');
+        inputResponse1.style.borderTopLeftRadius = '0';
+        inputResponse1.style.borderBottomLeftRadius = '0';
     }
 });
+
+error.addEventListener('click', ()=>{
+    error.classList
+    .remove('error-on');
+    error.classList.add('error');
+    inputResponse1.focus();
+    inputResponse1.value = '';
+})
 
 inputResponse1.addEventListener('focus', ()=>{
     inputResponse1.addEventListener('keydown', (e)=>{
         if(e.keyCode === 13){
-            console.log('Enter');
             e.preventDefault();
             const value = inputResponse1.value;
             if(value === '20'){
@@ -30,6 +44,12 @@ inputResponse1.addEventListener('focus', ()=>{
                 responseCorrect1.classList.add('response-correct-on-1');
                 responseCorrect1.innerHTML = value;
 
+            }else{
+                error.classList.remove('error');
+                error.classList
+                .add('error-on');
+                inputResponse1.style.borderTopLeftRadius = '0';
+                inputResponse1.style.borderBottomLeftRadius = '0';
             }
         }
     })
@@ -51,6 +71,8 @@ boxSelect.addEventListener('mouseenter', ()=>{
                 options.classList.remove('options-on');
                 options.classList.add('options');
                 responseCorrect2.innerHTML = value;
+            }else{
+                inputResponse2.value = value;
             }
         })
     });
@@ -62,3 +84,24 @@ content.addEventListener('mouseleave', ()=>{
     options.classList.add('options');
 });
 
+inputResponse2.addEventListener('click', ()=>{
+    options.classList.remove('options');
+    options.classList.add('options-on');
+    const optionsList = options.querySelectorAll('.option');
+
+    optionsList.forEach(option => {
+    option.addEventListener('click', ()=>{
+        const value = option.innerHTML;
+        if(value == 2){
+            inputResponse2.classList.remove('input-response-2');
+            inputResponse2.classList.add('response-off-2');
+            responseCorrect2.classList.add('response-correct-on-2');
+            options.classList.remove('options-on');
+            options.classList.add('options');
+            responseCorrect2.innerHTML = value;
+        }else{
+            inputResponse2.value = value;
+        }
+    })
+    });
+})
